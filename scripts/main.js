6aef47f79df4a0a5cbc5568d3201e16124b0b5c3
kristina <adssa@fsdf.gfh>
2022-06-20 16:50:29 +0300

    new updates to save

---
+++ scripts/main.js
@@ -0,0 +1,63 @@
+//alert(`Привет ${str}`);
+//alert('Привет ' + str);
+
+// let str = '';
+// if (!localStorage.getItem('name') || localStorage.getItem('name') === 'null') {
+//     str = prompt('Ваше имя');
+//     localStorage.setItem('name', str)
+// } else {
+//     str = localStorage.getItem('name');
+// }
+
+// alert("Привет, " + str);
+
+// console.log(window);
+
+// let userAge = '';
+// userAge = prompt ('Ваш возраст')
+// parsedAge = parseFloat (userAge);
+
+// if (parsedAge <= 0 || String(parsedAge) == 'NaN') {
+//     alert('Вы неправильно ввели данные');
+//     prompt ('Введите данные правильно');
+// } else {
+//   alert('Все мы знаем возраст не важен, если ты любишь котиков');
+// }
+
+
+// let cats = 1;
+// while (cats<=5) {
+//     alert ('Смотри, сколько котиков');
+// cats++;
+// }
+
+console.log(this);
+var bla = {
+    x: 0,
+    y: 0,
+    bla: function (){
+        console.log (this.y);
+    }
+}
+
+bla.y;
+bla.bla();
+
+// var num = 2;
+// function func() {
+//     var num = 10;
+//     console.log(num);
+//     num = 5;
+// }
+// func();
+// console.log(num);
+
+
+function func(x) {
+    console.log(typeof x);
+    console.log(arguments.length);
+}
+
+func();
+func('1','2','3');
+
